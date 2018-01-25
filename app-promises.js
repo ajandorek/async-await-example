f5687877551f7e6d22ca4d3080593d31d18b5d5a
Alec Jandorek <ajandorek@gmail.com>
2018-01-25 14:02:22 -0600

    Init Commit

---
+++ app-promises.js
@@ -0,0 +1,62 @@
+const users = [{
+  id: 1,
+  name: 'Alec',
+  schoolId: 101
+}, {
+  id: 2,
+  name: 'Joe',
+  schoolId: 999
+}];
+
+const grades = [{
+  id: 1,
+  schoolId: 101,
+  grade: 90
+}, {
+  id: 2,
+  schoolId: 999,
+  grade: 100
+}, {
+  id: 1,
+  schoolId: 101,
+  grade: 95
+}];
+
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    const user = users.find((user) => user.id === id);
+
+    if (user) {
+      resolve(user);
+    } else {
+      reject(`Unable to fine user with id of ${id}.`);
+    }
+  });
+};
+
+const getGrades = (schoolId) => {
+  return new Promise((resolve, reject) => {
+    resolve(grades.filter((grade) => grade.schoolId === schoolId));
+  });
+};
+
+const getStatus = (userId) => {
+  var user;
+  return getUser(userId).then((tempUser) => {
+    user = tempUser;
+    return getGrades(user.schoolId);
+  }).then((grades) => {
+    let average = 0;
+
+    if( grades.length > 0 ) {
+      average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+    }
+    return `${user.name} has a ${average}% average in the class`;
+  });
+}
+
+getStatus(2).then((status) => {
+  console.log(status);
+}).catch((e) => {
+  console.log(e);
+});
